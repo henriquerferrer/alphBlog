669a8bdc267cec2e0a8f7adefd576ed1e3389c0f
Henrique <hvrbferrer@gmail.com>
2017-12-27 06:03:25 +0000

    adds missing email regex in user model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: {minimum: 3, maximum: 25 }
 
-  VALID_EMAIL_REGEX =
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, presence: true,
             uniqueness: { case_sensitive: false },
